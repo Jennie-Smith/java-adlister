d3c7c7a670a46ca5bad29516501c3affdfaaa48b
Jennie Smith <Jsmith9spiral@gmail.com>
2023-03-07 13:47:39 -0600

    update

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,25 +1,18 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.servlet.ServletException;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<HTML>");
-        out.println("<HEAD> <TITLE> Hello World <TITLE> <HEAD>");
-        System.out.println("done");
+        out.println("<HEAD> <TITLE> Hello World </TITLE> ");
+        out.println("<BODY> <H1> Hello World! </H1> </BODY></HEAD>");
     }
 }
