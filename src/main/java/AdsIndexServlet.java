2223d952e24eabef1b89145c74f279e5900c20d8
Jennie Smith <Jsmith9spiral@gmail.com>
2023-03-13 11:50:06 -0500

    update

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import com.mysql.cj.jdbc.Driver;
 import java.util.List;
 
 @WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
