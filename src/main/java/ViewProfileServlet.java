d7a93b6f20ebc8a65c2e215c3f3c04d33ac22973
Jennie Smith <Jsmith9spiral@gmail.com>
2023-03-14 11:04:56 -0500

    update

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+//        if they are logged in they can go to the profile page
+        HttpSession session = request.getSession();
+//        grab x  make sure it's true
+        String username = (String) session.getAttribute("username");
+        String password = (String) session.getAttribute("password");
+
+
+        if (username.equals("admin") && username != null && password.equals("password") && password != null ) {
+            String user = (String) session.getAttribute("key");
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        } else {
+            response.sendRedirect("/login");
+        }
+
     }
 }
